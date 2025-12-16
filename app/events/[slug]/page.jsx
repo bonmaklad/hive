7868d0b32e6555f509cf575275b2baca040277de
Michael Law <michael@necta.nz>
2025-12-17 09:21:02 +1300

    s

--- app/events/[slug]/page.jsx
+++ app/events/[slug]/page.jsx
@@ -48,7 +48,7 @@ export default function EventPage({ params }) {
                             </a>
                         </div>
                     </div>
-                </div>
+                </div> 
             </header>
 
             <main>
